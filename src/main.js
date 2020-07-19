f2412e6827c3f9e5c0d016233ac151cce6bd1ee1
AlonBru <66993391+AlonBru@users.noreply.github.com>
2020-07-19 15:57:25 +0300

    better base function
    >sorted style file
    >added date functionality
    >added priority color coding
    >added counter functionality
    >rearranged add function for readability

--- src/main.js
+++ src/main.js
@@ -1,36 +1,61 @@
-// li template:
-//<todoPriority><todoTest><todoCreatedAt>
+// elements
 const list= document.getElementById('view');
 const control = document.getElementById('control');
 const input = document.getElementById('inputText');
 const prioritySelector =document.getElementById('prioritySelector');
 const addButton =document.getElementById('addButton');
+const counter= document.getElementById('counter')
+
+//useful resources
+const time= new Date();
+const year= time.getFullYear();
+const month= (time.getMonth()<9)? '0'+(time.getMonth()+1):time.getMonth()+1;
+const day= time.getDate();
+const todos= [];
+const priorityColours= {
+    1:'green',
+    2:'yellow',
+    3:'orange',
+    4:'red',
+    5:'maroon'
+}
+//functions
 
 const create= (name)=>{
     return document.createElement(name);
 }
 
-const addItem=()=>{
-    if (list.querySelector('#undoError')) list.removeChild(list.querySelector('#undoError'));
-    //creating relevant elements
+const addItem=(e)=>{
+    //container
     const item= create('div');
-    const todoPriority= create('div');
-    const todoText= create('div');
-    const todoTime= create('div');
-    //setting classes
-    item.className='todoContainer';
-    todoText.className= 'todoText';
-    todoPriority.className= 'todoPriority';
-    todoTime.className= 'todoCreatedAt';
-    //setting values
-    todoPriority.innerText= prioritySelector.value
-    todoText.innerText= input.value;
-    todoTime.innerText= `Added at: Date}`
-    //appending
-    item.appendChild(todoPriority);
-    item.appendChild(todoText);
-    item.appendChild(todoTime);
     list.appendChild(item)
+    item.className='todoContainer';
+    const done =create('button');
+    //priority
+    debugger
+    const todoPriority= create('div');
+    todoPriority.className= 'todoPriority';
+    todoPriority.style.background=priorityColours[prioritySelector.value];
+    todoPriority.style.border= (prioritySelector.value==='5')? '2px dotted white':'1px solid black'
+    todoPriority.innerText= prioritySelector.value;
+    item.appendChild(todoPriority);
+    //text
+    const todoText= create('div');
+    todoText.className= 'todoText';
+    todoText.innerText= input.value;
+    item.appendChild(todoText);
+    //time
+    const todoTime= create('div');
+    todoTime.className= 'todoCreatedAt';
+    todoTime.innerText= `Added at: ${year}-${month}-${day}`
+    item.appendChild(todoTime);
+    //counter
+    todos.push(item)
+    counter.innerText= `${todos.length} Things To Do`
+    //reset input fields
+    prioritySelector.value=''
+    input.value=null;
+    input.focus();
 }
 
 addButton.addEventListener('click',addItem)
